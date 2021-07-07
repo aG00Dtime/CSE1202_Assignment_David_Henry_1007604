f5037427fea85b009a6c5541a9d9972375a7d8cc
David Henry <77511476+aG00Dtime@users.noreply.github.com>
2021-07-07 09:16:47 -0400

    Updated for submission

--- modules/collision.py
+++ modules/collision.py
@@ -6,10 +6,10 @@ import math
 def hit(x1, y1, x2, y2, space):
     
     distance = math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2))
-   # print(distance)
+    # print(distance)
 
     if distance <= space:
-
+        print("HIT")
         return True
 
 
